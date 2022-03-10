275ac699cb475f6f2a8f820c32c9c304b0ae2e53
tommaso-michelon <76102698+tommaso-michelon@users.noreply.github.com>
2022-03-10 11:22:34 +0100

    reupload files
    fix problems

---
+++ src/main/java/it/synclab/login/LoginSpringbootApplication.java
@@ -0,0 +1,13 @@
+package it.synclab.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringbootApplication.class, args);
+	}
+
+}
